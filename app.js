97d6ca7829c4a96dddb36a11f46b581dfee9a816
Michael Oldenburg <Michael Oldenburg@DESKTOP-O65QB3K>
2024-02-08 20:26:18 -0600

    Setting up the initial items needed for our website

--- app.js
+++ app.js
@@ -5,20 +5,28 @@ const server = require('http').createServer(app);
 const passport = require('passport');
 const path = require('path');
 const mongoose = require('mongoose');
+const React = require('react');
+const babel= require('@babel/register');
+
+
+/*Local Remember to re-enable Mongo*/
 
 //serve static files from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Import Database Models
-const { } = require('./model');
+//const { } = require('./model');
 
 //Database Connection
-const {getDb, connectToDb } = require('./db');
-const { Server } = require('net');
+//const {getDb, connectToDb } = require('./db');
+//const { Server } = require('net');
 
 //Configure views and view engine
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
+
+//Babel middleware to transpile JSX
+app.engine('jsx', require('express-react-views').createEngine());
 
 //Listen to the server
 server.listen(process.env.PORT);
---
+++ views/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainComponent = () => {
+    return (
+        <div>
+            <h1>TESTING</h1>
+            {}
+        </div>
+    );
+};
+
+export default MainComponent;
