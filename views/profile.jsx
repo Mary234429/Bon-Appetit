eb13003d63aae01ac635a2e93da848f3580e99b8
SparkedHam <zachary.c@sparkedhost.com>
2024-02-19 00:25:20 -0600

    Update Pages

--- views/profile.jsx
+++ views/profile.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 const Profile = ({ title }) => (
     <div>
         <h1>{title}</h1>
-        <p>This is the profile page.</p>
+        <p>This is the Profile page.</p>
         <form action="/" method="get">
             <button type="submit">Go to Main Page</button>
         </form>
+
+
     </div>
 );
 
